ae6f3a6677aa4b3f2bd50198c796de856bb55bc1
William Waites <ww@groovy.net>
2017-08-30 08:42:41 +0100

    put filename properly in error message

--- ta/cmd.py
+++ ta/cmd.py
@@ -38,7 +38,7 @@ def main():
                     db.conn.commit()
                 except Exception:
                     exc_type, exc_value, exc_traceback = sys.exc_info()
-                    log.error("error processing %s:")
+                    log.error("error processing %s:", filename)
                     for s in traceback.format_exception(exc_type, exc_value, exc_traceback):
                         log.error(s)
                     db.conn.rollback()
